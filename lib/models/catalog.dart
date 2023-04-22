977a115e21f0b44508ca3a602e6cbda85c6a30b9
Ankit Middha <97609172+AnkitMiddha@users.noreply.github.com>
2023-04-22 22:16:06 +0530

    day10completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,9 @@
+class Item {
+  final String id;
+  final String name;
+  final num price;
+  final String desc;
+  final String color;
+
+  Item(this.id, this.name, this.price, this.desc, this.color);
+}
